afa8023b34d639d8573365d4513636f8a2bae057
SaadMushtaq7 <saad.mushtaq@emumba.com>
2022-06-13 15:54:56 +0500

    after formik commit

--- front-end/src/components/CreateGistFile.js
+++ front-end/src/components/CreateGistFile.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
+import { useFormik } from "formik";
 import NavBar from "./NavBar";
 import "react-toastify/dist/ReactToastify.css";
 import "../styles/create-gist-file.css";
@@ -10,17 +11,20 @@ export default function CreateGistFile() {
   const location = useLocation();
   const { user: currentUser } = location.state;
 
-  const [desc, setDesc] = useState("");
-  const [fileName, setFileName] = useState("");
-  const [fileContent, setFileContent] = useState("");
-
-  const createGist = () => {
-    if (desc !== "" && fileName !== "" && fileContent !== "") {
-      const file = { [fileName]: { content: fileContent } };
+  const formik = useFormik({
+    initialValues: {
+      description: "",
+      fileName: "",
+      fileContent: "",
+    },
+    onSubmit: () => {
+      const file = {
+        [formik.values.fileName]: { content: formik.values.fileContent },
+      };
       axios
         .post(
           "https://api.github.com/gists",
-          { files: file, description: desc },
+          { files: file, description: formik.values.description },
           {
             headers: {
               "Content-Type": "application/json",
@@ -30,55 +34,58 @@ export default function CreateGistFile() {
         )
         .then((res) => {
           toast.success("File Added Successfully!");
-          setDesc("");
-          setFileContent("");
-          setFileName("");
+          formik.setFieldValue("description", "");
+          formik.setFieldValue("fileName", "");
+          formik.setFieldValue("fileContent", "");
         })
         .catch((error) => {
           console.log(`Error adding data: ${error}`);
           toast.error("Failed to save data!");
         });
-    } else {
-      toast.error("Form not completely filled!");
-    }
-  };
+    },
+  });
 
   return (
     <>
       <NavBar user={currentUser} />
-      <div className="create-gist-file-container">
-        <div className="add-form">
-          <div className="description">
-            <input
-              placeholder="Enter gist description..."
-              value={desc}
-              onChange={(e) => setDesc(e.target.value)}
-            />
+      <form onSubmit={formik.handleSubmit}>
+        <div className="create-gist-file-container">
+          <div className="add-form">
+            <div className="description">
+              <input
+                name="description"
+                placeholder="Enter gist description..."
+                value={formik.values.description}
+                onChange={formik.handleChange}
+              />
+            </div>
+            <div className="filename">
+              <input
+                name="fileName"
+                placeholder="Enter file name..."
+                value={formik.values.fileName}
+                onChange={formik.handleChange}
+              />
+            </div>
+            <div className="filecontent">
+              <textarea
+                name="fileContent"
+                placeholder="Enter file content..."
+                rows="15"
+                cols="130"
+                value={formik.values.fileContent}
+                onChange={formik.handleChange}
+              />
+            </div>
           </div>
-          <div className="filename">
-            <input
-              placeholder="Enter file name..."
-              value={fileName}
-              onChange={(e) => setFileName(e.target.value)}
-            />
-          </div>
-          <div className="filecontent">
-            <textarea
-              placeholder="Enter file content..."
-              rows="15"
-              cols="130"
-              value={fileContent}
-              onChange={(e) => setFileContent(e.target.value)}
-            />
+          <div className="btn-action">
+            <button className="btn btn-success">Add File</button>
+            <button type="submit" className="btn btn-success">
+              Create Gist
+            </button>
           </div>
         </div>
-        <div className="btn-action">
-          <button className="btn btn-success">Add File</button>
-          <button className="btn btn-success" onClick={createGist}>
-            Create Gist
-          </button>
-        </div>
-      </div>
+      </form>
       <ToastContainer
         position="top-center"
         autoClose={2500}
